aa2891b6ec780797c8b03a1a5753bcb744f232ac
Михаил Красильников <m.krasilnikov@yandex.ru>
2017-02-20 22:31:09 +0300

    Доработан импорт БД

--- common.mk
+++ common.mk
@@ -2,11 +2,11 @@ SHELL = /bin/sh
 
 .PHONY: import-db
 
-REMOTE_PROTO ::= $($(REMOTE)_proto)
-REMOTE_HOST ::= $($(REMOTE)_$(REMOTE_PROTO)_host)
-REMOTE_USER ::= $($(REMOTE)_$(REMOTE_PROTO)_user)
-REMOTE_PASSWORD ::= $($(REMOTE)_$(REMOTE_PROTO)_password)
-REMOTE_ROOT ::= $($(REMOTE)_$(REMOTE_PROTO)_root)
+REMOTE_PROTO := $($(REMOTE)_proto)
+REMOTE_HOST := $($(REMOTE)_$(REMOTE_PROTO)_host)
+REMOTE_USER := $($(REMOTE)_$(REMOTE_PROTO)_user)
+REMOTE_PASSWORD := $($(REMOTE)_$(REMOTE_PROTO)_password)
+REMOTE_ROOT := $($(REMOTE)_$(REMOTE_PROTO)_root)
 
 ##
 # Проверяет что указанные переменные установлены и их значения не пусты.
@@ -22,13 +22,14 @@ __assert_variable_set = $(if $(value $1),,$(error Undefined variable $1$(if $2, 
 
 import-db:
 	$(call assert_variable_set, REMOTE, имя конфигурации сайта)
+	$(call assert_variable_set, LOCAL_DB_NAME, имя локальной БД)
 	$(if $(REMOTE_HOST),,$(error Undefined variable $(REMOTE)_$(REMOTE_PROTO)_host))
 	$(if $(REMOTE_USER),,$(error Undefined variable $(REMOTE)_$(REMOTE_PROTO)_user))
 	$(if $(REMOTE_PASSWORD),,$(error Undefined variable $(REMOTE)_$(REMOTE_PROTO)_password))
 	$(if $(REMOTE_ROOT),,$(error Undefined variable $(REMOTE)_$(REMOTE_PROTO)_root))
-	$(eval tmp_file ::= $(shell mktemp --tmpdir import-db.XXXX))
+	$(eval tmp_file := $(shell mktemp --tmpdir import-db.XXXX))
 ifeq ($(REMOTE_PROTO),ftp)
-	curl --upload-file ../.site/mysqldump.php ftp://$(REMOTE_HOST)$(REMOTE_ROOT) \
+	curl --upload-file ../.dev-tools/mysqldump.php ftp://$(REMOTE_HOST)$(REMOTE_ROOT) \
 		--user $(REMOTE_USER):$(REMOTE_PASSWORD)
 	curl --data 'user=$(prod_db_user)&password=$(prod_db_password)&db=$(prod_db_name)&host=$(prod_db_host)' \
 		$(prod_http_root)/mysqldump.php > $(tmp_file)
@@ -43,5 +44,9 @@ else
 	mysql --user=$(DB_USER) --password=$(DB_PASSWORD) $(DB_NAME) < /tmp/$(test_db_name).sql
 	rm /tmp/$(test_db_name).sql
 endif
-	mysql
+ifdef LOCAL_DB_USER
+	mysql --user=$(LOCAL_DB_USER) --password=$(LOCAL_DB_PASSWORD) $(LOCAL_DB_NAME) < $(tmp_file)
+else
+	mysql $(LOCAL_DB_NAME) < $(tmp_file)
+endif
 	-rm $(tmp_file)
