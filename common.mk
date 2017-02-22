d0aab5e6ff9dd4d18a0843ac6316da84d86b9559
Михаил Красильников <m.krasilnikov@yandex.ru>
2017-02-22 12:42:04 +0300

    Поддержка SSH в import-db

--- common.mk
+++ common.mk
@@ -8,6 +8,14 @@ REMOTE_USER := $($(REMOTE)_$(REMOTE_PROTO)_user)
 REMOTE_PASSWORD := $($(REMOTE)_$(REMOTE_PROTO)_password)
 REMOTE_ROOT := $($(REMOTE)_$(REMOTE_PROTO)_root)
 
+REMOTE_DB_NAME := $($(REMOTE)_db_name)
+REMOTE_DB_HOST := $($(REMOTE)_db_host)
+ifeq ($(REMOTE_DB_HOST),)
+REMOTE_DB_HOST := localhost
+endif
+REMOTE_DB_USER := $($(REMOTE)_db_user)
+REMOTE_DB_PASSWORD := $($(REMOTE)_db_password)
+
 ##
 # Проверяет что указанные переменные установлены и их значения не пусты.
 # В случае ошибки прерывает работу сценария.
@@ -24,11 +32,11 @@ import-db:
 	$(call assert_variable_set, REMOTE, имя конфигурации сайта)
 	$(call assert_variable_set, LOCAL_DB_NAME, имя локальной БД)
 	$(if $(REMOTE_HOST),,$(error Undefined variable $(REMOTE)_$(REMOTE_PROTO)_host))
+ifeq ($(REMOTE_PROTO),ftp)
+	$(if $(REMOTE_ROOT),,$(error Undefined variable $(REMOTE)_$(REMOTE_PROTO)_root))
 	$(if $(REMOTE_USER),,$(error Undefined variable $(REMOTE)_$(REMOTE_PROTO)_user))
 	$(if $(REMOTE_PASSWORD),,$(error Undefined variable $(REMOTE)_$(REMOTE_PROTO)_password))
-	$(if $(REMOTE_ROOT),,$(error Undefined variable $(REMOTE)_$(REMOTE_PROTO)_root))
 	$(eval tmp_file := $(shell mktemp --tmpdir import-db.XXXX))
-ifeq ($(REMOTE_PROTO),ftp)
 	curl --upload-file ../.dev-tools/mysqldump.php ftp://$(REMOTE_HOST)$(REMOTE_ROOT) \
 		--user $(REMOTE_USER):$(REMOTE_PASSWORD)
 	curl --data 'user=$(prod_db_user)&password=$(prod_db_password)&db=$(prod_db_name)&host=$(prod_db_host)' \
@@ -36,13 +44,12 @@ ifeq ($(REMOTE_PROTO),ftp)
 	-curl ftp://$(REMOTE_HOST)$(REMOTE_ROOT) --request 'DELE mysqldump.php' \
 		--user $(REMOTE_USER):$(REMOTE_PASSWORD)
 else
-	ssh $(test_host) \
-		'mysqldump --user=$(test_db_user) --password=$(test_db_password) $(test_db_name) | xz > /tmp/$(test_db_name).sql.xz'
-	scp $(test_host):/tmp/$(test_db_name).sql.xz /tmp/
-	-rm /tmp/$(test_db_name).sql
-	xz -d /tmp/$(test_db_name).sql.xz
-	mysql --user=$(DB_USER) --password=$(DB_PASSWORD) $(DB_NAME) < /tmp/$(test_db_name).sql
-	rm /tmp/$(test_db_name).sql
+	ssh $(REMOTE_HOST) \
+		'mysqldump --user=$(REMOTE_DB_USER) --password=$(REMOTE_DB_PASSWORD) $(REMOTE_DB_NAME) | xz > /tmp/$(REMOTE_DB_NAME).sql.xz'
+	scp $(REMOTE_HOST):/tmp/$(REMOTE_DB_NAME).sql.xz /tmp/
+	$(eval tmp_file := /tmp/$(REMOTE_DB_NAME).sql)
+	-rm $(tmp_file)
+	xz -d /tmp/$(REMOTE_DB_NAME).sql.xz
 endif
 ifdef LOCAL_DB_USER
 	mysql --user=$(LOCAL_DB_USER) --password=$(LOCAL_DB_PASSWORD) $(LOCAL_DB_NAME) < $(tmp_file)
