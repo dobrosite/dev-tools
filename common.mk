7c4562ee74d7b9be03c65e54b5fc3bc55ffc6199
Михаил Красильников <m.krasilnikov@yandex.ru>
2017-03-07 01:01:33 +0300

    Экспорт БД на удалённый сервер.

--- common.mk
+++ common.mk
@@ -28,6 +28,9 @@ assert_variable_set = $(strip $(foreach 1,$1, \
 
 __assert_variable_set = $(if $(value $1),,$(error Undefined variable $1$(if $2, ($2))))
 
+##
+# Импортирует БД с удалённого сервера на локальный.
+#
 import-db:
 	$(call assert_variable_set, REMOTE, имя конфигурации сайта)
 	$(call assert_variable_set, LOCAL_DB_NAME, имя локальной БД)
@@ -55,5 +58,45 @@ ifdef LOCAL_DB_USER
 	mysql --user=$(LOCAL_DB_USER) --password=$(LOCAL_DB_PASSWORD) $(LOCAL_DB_NAME) < $(tmp_file)
 else
 	mysql $(LOCAL_DB_NAME) < $(tmp_file)
+endif
+	-rm $(tmp_file)
+
+##
+# Экспортирует БД с локального сервера на удалённый.
+#
+# ВНИМАНИЕ! Во избежание потери данных, экспорт на боевой сайт не поддерживается!
+#
+export-db:
+	$(call assert_variable_set, REMOTE, имя конфигурации сайта)
+ifeq ($(REMOTE),prod)
+	$(error Export to production server is prohibited!)
+endif
+	$(call assert_variable_set, LOCAL_DB_NAME, имя локальной БД)
+	$(if $(REMOTE_HOST),,$(error Undefined variable $(REMOTE)_$(REMOTE_PROTO)_host))
+	$(eval tmp_file := $(shell mktemp --tmpdir export-db.XXXX))
+	$(eval tmp_basename := $(shell basename $(tmp_file)))
+ifdef LOCAL_DB_USER
+	mysqldump --user=$(LOCAL_DB_USER) --password=$(LOCAL_DB_PASSWORD) $(LOCAL_DB_NAME) > $(tmp_file)
+else
+	mysqldump $(LOCAL_DB_NAME) > $(tmp_file)
+endif
+ifeq ($(REMOTE_PROTO),ftp)
+	$(error Export over FTP is not supported yet!)
+	$(if $(REMOTE_ROOT),,$(error Undefined variable $(REMOTE)_$(REMOTE_PROTO)_root))
+	$(if $(REMOTE_USER),,$(error Undefined variable $(REMOTE)_$(REMOTE_PROTO)_user))
+	$(if $(REMOTE_PASSWORD),,$(error Undefined variable $(REMOTE)_$(REMOTE_PROTO)_password))
+	$(eval tmp_file := $(shell mktemp --tmpdir import-db.XXXX))
+	curl --upload-file ../.dev-tools/mysqldump.php ftp://$(REMOTE_HOST)$(REMOTE_ROOT) \
+		--user $(REMOTE_USER):$(REMOTE_PASSWORD)
+	curl --data 'user=$(prod_db_user)&password=$(prod_db_password)&db=$(prod_db_name)&host=$(prod_db_host)' \
+		$(prod_http_root)/mysqldump.php > $(tmp_file)
+	-curl ftp://$(REMOTE_HOST)$(REMOTE_ROOT) --request 'DELE mysqldump.php' \
+		--user $(REMOTE_USER):$(REMOTE_PASSWORD)
+else
+	xz $(tmp_file)
+	scp $(tmp_file).xz $(REMOTE_HOST):/tmp/
+	ssh $(REMOTE_HOST) \
+		'xzcat /tmp/$(tmp_basename).xz | mysql --user=$(REMOTE_DB_USER) --password=$(REMOTE_DB_PASSWORD) $(REMOTE_DB_NAME)'
+	-rm $(tmp_file).xz
 endif
 	-rm $(tmp_file)
